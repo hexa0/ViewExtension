081b8876d704a0544eea07bd3840d15bb1158c07
sfDesat <75356901+sfDesat@users.noreply.github.com>
2024-04-27 20:14:11 +0200

    Delete CameraCullingIncrease directory

--- CameraCullingIncrease/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CameraCullingIncrease
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "CameraCullingIncrease";
-        public const string PLUGIN_NAME = "CameraCullingIncrease";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
