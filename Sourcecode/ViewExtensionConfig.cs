df2f01c8a5914166902be2fd3b4757d5db0b4ea4
sfDesat <75356901+sfDesat@users.noreply.github.com>
2024-06-17 11:07:23 +0200

    Update ViewExtensionConfig.cs

--- Sourcecode/ViewExtensionConfig.cs
+++ Sourcecode/ViewExtensionConfig.cs
@@ -3,19 +3,10 @@ using BepInEx.Configuration;
 
 public static class ViewExtensionConfig
 {
-    // Define a configuration option for FarClipPlane
     public static ConfigEntry<float> FarClipPlane;
 
     internal static void Init(ConfigFile config)
     {
-        // Define the configuration option for FarClipPlane with a default value of 1500f
-        FarClipPlane = config.Bind("General", "FarClipPlane", 1500f, "View distance in units. Vanilla is 400.");
-
-        if (float.IsNaN(FarClipPlane.Value) || FarClipPlane.Value <= 0 || FarClipPlane.Value >= 100000)
-        {
-            // If so, log a warning and set a default value
-            Debug.LogWarning("ViewExtension: Invalid value for FarClipPlane in the config file. Using default value of 1500.");
-            FarClipPlane.Value = 1500f; // Set a default value
-        }
+        FarClipPlane = config.Bind("General", "FarClipPlane", 1500f, "View distance in units. Vanilla is 400. (between 1 and 99999)");
     }
 }
