d443be0bf436fe2aebd5bb74f01498219269242d
sfDesat <75356901+sfDesat@users.noreply.github.com>
2024-06-18 15:15:45 +0200

    v1.3.0

--- Sourcecode/ViewExtensionConfig.cs
+++ Sourcecode/ViewExtensionConfig.cs
@@ -1,12 +1,22 @@
-using UnityEngine;
 using BepInEx.Configuration;
 
 public static class ViewExtensionConfig
 {
     public static ConfigEntry<float> FarClipPlane;
+    public static ConfigEntry<bool> MenuOption { get; set; }
+    public static ConfigEntry<bool> DebugLogging { get; set; }
+    internal static ConfigFile Config { get; private set; }
 
     internal static void Init(ConfigFile config)
     {
-        FarClipPlane = config.Bind("General", "FarClipPlane", 1500f, "View distance in units. Vanilla is 400. (between 1 and 99999)");
+        Config = config;
+        FarClipPlane = config.Bind("1. General", "FarClipPlane", 1000f, "View distance in units. Vanilla is 400. (between 1 and 10000)");
+        MenuOption = config.Bind("1. General", "MenuOption", true, "Whether to add an option in the settings menu to change the view distance.");
+        DebugLogging = config.Bind("2. Debugging", "DebugLogging", false, "Whether to display debug messages.");
+    }
+
+    public static void Save()
+    {
+        Config.Save();
     }
 }
